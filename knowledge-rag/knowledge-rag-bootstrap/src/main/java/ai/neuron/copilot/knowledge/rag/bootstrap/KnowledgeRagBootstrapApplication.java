70279a1a12cc6c455dcf9ae34946d667e21a3497
Ken <kennsongyou@gmail.com>
2026-02-23 11:42:07 +0800

    init

--- knowledge-rag/knowledge-rag-bootstrap/src/main/java/ai/neuron/copilot/knowledge/rag/bootstrap/KnowledgeRagBootstrapApplication.java
+++ knowledge-rag/knowledge-rag-bootstrap/src/main/java/ai/neuron/copilot/knowledge/rag/bootstrap/KnowledgeRagBootstrapApplication.java
@@ -3,7 +3,10 @@ package ai.neuron.copilot.knowledge.rag.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ai.neuron.copilot.knowledge")
+@SpringBootApplication(scanBasePackages = {
+		"ai.neuron.copilot.knowledge.rag",
+		"ai.neuron.copilot.knowledge.foundation"
+})
 public class KnowledgeRagBootstrapApplication {
 
 	public static void main(String[] args) {
